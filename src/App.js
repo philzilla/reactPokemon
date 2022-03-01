37d9dc15ce28b19c64e6d4d44d8b3c86f49d607e
philzilla <bonjour@philippeyim.fr>
2022-03-01 10:23:37 +0100

    ajouter la carte leaflet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Fragment } from 'react'
+import {Map} from './componants/Map'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="row">
         <div className="col">
           <h1>Pokémon Go !</h1>
+          <Map />
         </div>
       </div>
     </div>
--- src/componants/Map.js
+++ src/componants/Map.js
@@ -1,3 +1,10 @@
+import { MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+import { useEffect, useState } from "react"
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
 
 export const Map = () => {
   // code JS 👇
@@ -7,9 +14,39 @@ export const Map = () => {
     // Code JS 👇
 
 
-    <> 
+    <div style={{ width: '80%', height: '600px', margin: '0 auto'}}>
     {/* HTML 👇 */}
+
+    <MapContainer
+        center={[46.6681699, -1.4148661]}
+        zoom={12}
+        scrollWheelZoom={false} // Zoomer avec le scroll de la souris
+        style={{ height: "100%", width: "100%" }}
+      >
+
+        <TileLayer
+          url={"https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYnVudGhlYXIiLCJhIjoiY2tkYTdnOHpmMGI3NDJxbXpoc2QwMXc3MyJ9.nu-giQ821MNuH64prgx2yg"}
+          attribution='Pokemon Go !' 
+        />
+          <Marker
+            position={[46.6681699, -1.4148661]}
+            draggable={true}
+            icon={
+              L.icon({
+                iconSize: [60, 60],
+                setRadius: 10,
+                iconAnchor: [10, 41],
+                popupAnchor: [2, -40],
+                shadowSize: [100, 30], // size of the shadow
+                shadowAnchor: [15, 10],  // the same for the shadow
+                iconUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png`,
+                shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png"
+              })
+            }>
+            <Popup>Bulbi</Popup>
+          </Marker>
+      </MapContainer>
     
-    </>
+    </div>
   )
 }
